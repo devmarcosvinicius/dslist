6a7eb1a3df1a7577f82ab1a4ffc0ae1b796923e3
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-11-13 22:18:18 -0300

    Creating findAll method, adding controller annotations

--- src/main/java/com/marcosviniciusdev/controllers/GameController.java
+++ src/main/java/com/marcosviniciusdev/controllers/GameController.java
@@ -1,4 +1,24 @@
 package com.marcosviniciusdev.controllers;
 
+import com.marcosviniciusdev.entities.Game;
+import com.marcosviniciusdev.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
 public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<Game> findAll() {
+        return gameService.findAll();
+    }
+
 }
