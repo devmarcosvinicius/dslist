c75ab2f50cc296123a101969a7e3bf962230e1f6
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-11-13 22:16:02 -0300

    Creating findAll Method

--- src/main/java/com/marcosviniciusdev/services/GameService.java
+++ src/main/java/com/marcosviniciusdev/services/GameService.java
@@ -1,4 +1,20 @@
 package com.marcosviniciusdev.services;
 
+import com.marcosviniciusdev.entities.Game;
+import com.marcosviniciusdev.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<Game> findAll() {
+        return gameRepository.findAll();
+    }
+
 }
