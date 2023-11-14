f28345f26e87ece4db24fa4189fb5e0b95999ec2
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-11-13 22:04:21 -0300

    Extending to JpaRepository

--- src/main/java/com/marcosviniciusdev/repositories/GameRepository.java
+++ src/main/java/com/marcosviniciusdev/repositories/GameRepository.java
@@ -1,4 +1,7 @@
 package com.marcosviniciusdev.repositories;
 
-public interface GameRepository {
+import com.marcosviniciusdev.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
 }
